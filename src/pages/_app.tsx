39603999dafba1bc66d40dcc7243d6e70fbb9f52
Srujan Gurram <srujangs8@gmail.com>
2023-05-02 22:12:12 +0530

    setup basic wagmi magic connector 🚀

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,33 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { MagicAuthConnector } from "@everipedia/wagmi-magic-connector";
+import type { AppProps } from "next/app";
+import { WagmiConfig, configureChains, createClient, mainnet } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, provider, webSocketProvider } = configureChains(
+	[mainnet],
+	[publicProvider()],
+);
+
+const client = createClient({
+	autoConnect: true,
+	provider,
+	webSocketProvider,
+	connectors: [
+		new MagicAuthConnector({
+			chains,
+			options: {
+				apiKey: process.env.NEXT_PUBLIC_MAGIC_API_KEY as string,
+				isDarkMode: true,
+			},
+		}),
+	],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<WagmiConfig client={client}>
+			<Component {...pageProps} />
+		</WagmiConfig>
+	);
 }
